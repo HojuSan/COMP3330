6a4e8ec67ec31224ac1e4f62623ba26fe1e178a0
HojuSan <juyong.kim@uon.edu.au>
2019-02-27 14:05:43 +1100

    first commit

---
+++ PythonCrashCourse.py
@@ -0,0 +1,10 @@
+a = 1 #a is an integer with a value of 1
+a += 1 #equivalent to a = a + 1 (python does not have a ++ operator)
+a *= 2 #equivalent to a = a * 2
+print(a, type(a))
+
+b = 1. #a is now a float -- variable types can be changed
+print(b, type(a))
+
+b = 1.5
+print(a, type(a))
